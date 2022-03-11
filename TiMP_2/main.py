793ce83441729758788d93aeac37dc8c4fec4437
chiririll <sstive39@gmail.com>
2022-03-12 00:09:05 +0300

    final

--- TiMP_2/main.py
+++ TiMP_2/main.py
@@ -7,11 +7,6 @@ def main():
         'TOP': [1, 2, 8, 11],
         'SEQUENCE': "I2I3I3"
     }
-    task_test_1 = {
-        'BASE': [-1, 3, 9, 9],
-        'TOP': [0, 6, 9, 10],
-        'SEQUENCE': "I1I4I4I1I4I2I4I4I1I1"
-    }
 
     laba = Laba2(task_test)
     laba.run()
